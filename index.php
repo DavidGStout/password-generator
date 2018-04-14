afea1c95c00d4eba26a257a8f6540fb900a3d6d4
David Stout <davidgstout63@gmail.com>
2018-04-14 23:05:18 +0000

    Added extraction of configuration flags into variables.

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-    $lower = 'abcdef';
-    $upper = 'ABCDEF';
-    $numbers = '0123456789';
-    $lorem = '*?!-';
-    
-    $chars = $lower . $upper . $numbers . $lorem;
     
     function random_char($string) {
     $i = mt_rand(0, strlen($string)-1);
@@ -22,6 +16,30 @@
     return $output;
     }
     
-    echo random_string(8, $chars);
     
+    function generate_password($length) {
+    // define character sets
+    $lower = 'abcdef';
+    $upper = 'ABCDEF';
+    $numbers = '0123456789';
+    $symbols = '*?!-';
+    
+    // extract configuration flags into variables.
+    $use_lower = true;
+    $use_upper = true;
+    $use_numbers = true;
+    $use_sumbols = true;
+    
+    $chars = '';
+    if($use_lower === true) {$chars .= $lower; }
+    if($use_upper === true) {$chars .= $upper; }
+    if($use_numbers === true) {$chars .= $numbers; }
+    if($use_symbols === true) {$chars .= $symbols; }
+
+
+        return random_string($length, $chars);
+    }
+    
+    echo generate_password(99);
+
 ?>
